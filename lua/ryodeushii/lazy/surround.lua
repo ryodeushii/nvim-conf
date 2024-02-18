058db23325bff0407c8e8d23f536d179227a55fd
Evgeny K <3193631@gmail.com>
2024-02-18 19:48:15 +0100

    chore: update  bindings comment

--- lua/ryodeushii/lazy/surround.lua
+++ lua/ryodeushii/lazy/surround.lua
@@ -4,5 +4,8 @@ return {
         require("nvim-surround").setup({})
         -- S in visual mode
         -- ys<motion><surround> to add surround
+        -- cs<old><new> to change surround for nearest <old> to <new>
+        -- csq<char> change nearest quotes pair to <char>
+        -- csqb change nearest quotes pair to brackets
     end,
 }
