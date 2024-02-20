75eed8bf788ee9c8f0acaf9386ad68af5f4ac908
Evgeny K <3193631@gmail.com>
2024-02-20 10:24:39 +0100

    feat:  remove github.copilot and use zbirenbaum/copilot.lua

---
+++ lua/ryodeushii/lazy/copilot.lua
@@ -0,0 +1,33 @@
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+
+            },
+            filetypes = {
+                javascript = true,
+                typescript = true,
+                rust = true,
+                lua = true,
+                perl = true,
+                go = true,
+                yaml = true,
+                json = true,
+                toml = true,
+                python = true,
+                markdown = true,
+                vim = true,
+                sh = true,
+                bash = true,
+                ["*"] = false,
+            }
+        })
+    end,
+}
--- lua/ryodeushii/lazy/init.lua
+++ lua/ryodeushii/lazy/init.lua
@@ -4,8 +4,6 @@ return {
         "nvim-lua/plenary.nvim",
         name = "plenary"
     },
-
-    "github/copilot.vim",
     "eandrju/cellular-automaton.nvim",
     "gpanders/editorconfig.nvim",
 }
