a7c3fc291304fd7d4fc79d08ca7539ba863c678c
Evgeny K <3193631@gmail.com>
2024-02-22 19:39:10 +0100

    feat: add nvimux to use tmux multiplexer inside  of  nvim

---
+++ lua/ryodeushii/lazy/nvimux.lua
@@ -0,0 +1,18 @@
+return {
+    "hkupty/nvimux",
+
+    config = function()
+        local nvimux = require("nvimux")
+        nvimux.setup({
+            config = {
+                prefix = '<C-a>',
+            },
+            bindings = {
+                { { 'n', 'v', 'i', 't' }, 's', nvimux.commands.horizontal_split },
+                { { 'n', 'v', 'i', 't' }, 'v', nvimux.commands.vertical_split },
+            }
+
+        })
+    end,
+
+}
