f4cb2937317293602267a2d89d946cdd5830ac63
Evgeny K <3193631@gmail.com>
2024-02-27 10:38:22 +0100

    feat: enable code folding

--- lua/ryodeushii/set.lua
+++ lua/ryodeushii/set.lua
@@ -30,3 +30,13 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.opt.foldlevel = 20
+
+-- zc - Close (fold) the current fold under the cursor.
+-- zo - Open (unfold) the current fold under the cursor.
+-- za - Toggle between closing and opening the fold under the cursor.
+-- zR - Open all folds in the current buffer.
+-- zM - Close all folds in the current buffer.
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
